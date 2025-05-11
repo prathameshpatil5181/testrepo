f57c01506fb4d3b9c18187687fb34e04a7060743
prathameshpatil5181 <prathameshpatil5181@gmail.com>
2025-05-11 22:00:53 +0530

    deploy commit

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request } from "express";
+import cors from "cors";
+const app = express();
+
+app.use(cors({
+    origin:"*"
+}));
+
+
+
+app.get("/", (req:Request, res) => {
+  res.json({
+    stauts: 200,
+    msg:"Hello from the backend!"
+});
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
